5acd9e400aff9019f77ee926ffbee227e2df59e2
Bran-film <76977177+Bran-film@users.noreply.github.com>
2023-05-17 10:44:22 +0200

    Updates

---
+++ precache-manifest.1c404ae8d76f092057171258e5e90deb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1639c598d0a08c9581a9",
+    "url": "/coffee-dice/static/css/main.4c940117.chunk.css"
+  },
+  {
+    "revision": "1639c598d0a08c9581a9",
+    "url": "/coffee-dice/static/js/main.1639c598.chunk.js"
+  },
+  {
+    "revision": "9ad960da41216fce2577",
+    "url": "/coffee-dice/static/js/1.9ad960da.chunk.js"
+  },
+  {
+    "revision": "d89f92a25e620fbffcc2",
+    "url": "/coffee-dice/static/js/2.d89f92a2.chunk.js"
+  },
+  {
+    "revision": "55c595b0b508f7a77fdc",
+    "url": "/coffee-dice/static/js/runtime~main.55c595b0.js"
+  },
+  {
+    "revision": "853b4a0e07398f001679d1741e6261a5",
+    "url": "/coffee-dice/index.html"
+  }
+];
